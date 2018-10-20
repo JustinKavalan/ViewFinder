48e849524b93a6358fbee074db0f27949224558c
Justin Kavalan <justinkavalan@gmail.com>
2018-10-20 05:39:21 -0400

    Added functionality to write image to database

--- app/src/main/java/com/nosleep/viewfinder/util/FirebaseManager.java
+++ app/src/main/java/com/nosleep/viewfinder/util/FirebaseManager.java
@@ -1,6 +1,8 @@
 package com.nosleep.viewfinder.util;
 
+import android.graphics.Bitmap;
 import android.media.Image;
+import android.util.Log;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,27 +15,27 @@ import java.util.Objects;
 
 public class FirebaseManager {
 
-    public static String pushToImageContent(ImageData data) {
+    public static String pushToImageData(ImageData data) {
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference content = db.getReference("Metadata");
         content.push().setValue(data);
-        String pushId = content.getKey();
+        String pushId = content.push().getKey();
         return pushId;
     }
 
-    private static void pushToImage (Image image, String pushId) {
+    public static void pushToImageContent(String image, String pushId) {
         //TODO: Check for null
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference content = db.getReference("Image");
         DatabaseReference childContent = content.child(pushId);
+        Log.d("STRINGSIZE", "" + image.length());
         childContent.setValue(image);
-
     }
 
     public static void pushImage(DBImage img){
         //TODO: Check for null
-        String pushId = pushToImageContent(img.getImageData());
-        pushToImage(img.getImage(), pushId);
+        String pushId = pushToImageData(img.getImageData());
+        pushToImageContent(img.getImage(), pushId);
     }
 
     public List<Object> getClosestImages (double latitude, double longitude, int numOfImg) {
--- app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
@@ -4,11 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
+import com.nosleep.viewfinder.dbobject.DBImage;
 import com.nosleep.viewfinder.dbobject.ImageData;
 import com.nosleep.viewfinder.util.FirebaseManager;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends Activity {
 
@@ -17,12 +27,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_details);
 
+        Bitmap test = BitmapFactory.decodeResource(getResources(), R.drawable.night_sky_1);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        test.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        String result =  Base64.encodeToString(baos.toByteArray(), Base64.URL_SAFE);
+        DBImage test1 = new DBImage(new ImageData(), result);
+        FirebaseManager.pushImage(test1);
+
         ViewPager viewPager = (ViewPager) findViewById(R.id.vp_location_images);
         viewPager.setAdapter(new CustomPageAdapter(this));
 
-        ImageData imd = new ImageData();
-        FirebaseManager.pushToImageContent(imd);
-        setContentView(R.layout.activity_main);
     }
 
     /** Called when user selects location from feed */
