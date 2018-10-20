a4f08a5891d31d80fcb30de0e3cc3ba1eab58d4b
mattcar15 <mwcarroll12@gmail.com>
2018-10-20 09:06:38 -0400

    added the functionality to the UI

--- app/src/main/java/com/nosleep/viewfinder/viewfinder/Post.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/Post.java
@@ -2,12 +2,52 @@ package com.nosleep.viewfinder.viewfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+
+import com.google.android.gms.maps.MapView;
+
+import java.io.Console;
 
 public class Post extends AppCompatActivity {
+
+    Button btnClose;
+    Button btnPost;
+    ImageView ivPostPreview;
+    EditText etCaption;
+    RatingBar rbRating;
+    double longitude;
+    double latitude;
+    double star = 5.0;
+    String caption = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        btnClose = findViewById(R.id.btnClose);
+        btnPost = findViewById(R.id.btnPost);
+        ivPostPreview = findViewById(R.id.ivPostPreview);
+        etCaption = findViewById(R.id.etCaption);
+        rbRating = findViewById(R.id.rbRating);
+
+        rbRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                star = rbRating.getRating();
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                caption = etCaption.getText().toString();
+                //Where the post is sent to firebase
+            }
+        });
     }
 }
