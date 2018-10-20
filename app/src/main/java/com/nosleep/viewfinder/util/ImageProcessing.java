8765d2204aa33f3561594607bf13b4b3fdd7eca7
Justin Kavalan <justinkavalan@gmail.com>
2018-10-20 08:34:06 -0400

    Write decoder function

--- app/src/main/java/com/nosleep/viewfinder/util/ImageProcessing.java
+++ app/src/main/java/com/nosleep/viewfinder/util/ImageProcessing.java
@@ -1,7 +1,9 @@
 package com.nosleep.viewfinder.util;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Base64;
+import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 
@@ -12,8 +14,14 @@ public class ImageProcessing {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         input.compress(Bitmap.CompressFormat.JPEG, 100, baos);
         String result =  Base64.encodeToString(baos.toByteArray(), Base64.URL_SAFE);
+        Log.d("RESULT", "" + result.length());
 
         return result;
+    }
+
+    public static Bitmap convertStringToBitmap(String input) {
+        byte[] decodedBytes = Base64.decode(input, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
     }
 
 }
