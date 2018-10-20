9db5ea809826ad1f79f8437f2d8af600b9d32394
Siddhartha Vemuri <svemuri8@gatech.edu>
2018-10-20 15:07:31 -0400

    Post image still in progress

--- app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
@@ -2,19 +2,29 @@ package com.nosleep.viewfinder.viewfinder;
 
 import android.app.Activity;
 import android.content.Intent;
+
+import androidx.core.content.FileProvider;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.nosleep.viewfinder.util.FirebaseManager;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,7 +57,6 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -74,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
 //        FirebaseManager.pushImage(null);
     }
 
+
     /** Calls phone camera to take picture */
     //Call this method after plus sign is added to menu bar
     private void dispatchTakePictureIntent() {
@@ -88,12 +98,62 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView img = (ImageView) findViewById(R.id.ivPostPreview);
+            System.out.println(imageBitmap);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+            ImageView img = (ImageView) findViewById(R.id.imageView2);
             img.setImageBitmap(imageBitmap);
         }
-        Intent intent = new Intent(this, Post.class);
-        setContentView(R.layout.activity_post);
-        startActivity(intent);
     }
 
+
+//    String mCurrentPhotoPath;
+//
+//    private File createImageFile() throws IOException {
+//        // Create an image file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+//
+//        // Save a file: path for use with ACTION_VIEW intents
+//        mCurrentPhotoPath = image.getAbsolutePath();
+//        return image;
+//    }
+//
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//                Intent errorIntent = new Intent(this, MainActivity.class);
+//                startActivity(errorIntent);
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//
+//                Intent intent = new Intent(this, Post.class);
+//                startActivity(intent);
+//
+//                ImageView img = (ImageView) findViewById(R.id.ivPostPreview);
+//                img.setImageURI(photoURI);
+//            }
+//        }
+//    }
 }
