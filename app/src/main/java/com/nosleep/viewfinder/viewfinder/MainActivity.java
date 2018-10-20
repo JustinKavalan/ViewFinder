00fe117ed1b92d07c1db2cb09688220a4d0bf5b2
Marc D'Mello <marcd2000@gmail.com>
2018-10-19 22:10:09 -0400

    Removed google-services.json file from gitignore so project can build

--- app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
@@ -3,11 +3,17 @@ package com.nosleep.viewfinder.viewfinder;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = db.getReference("message");
+        myRef.setValue("Hello World!");
         setContentView(R.layout.activity_main);
     }
 }
