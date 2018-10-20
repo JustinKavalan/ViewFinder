afc83514a702a412ca4d6f64874241b09bf9aa9e
Siddhartha Vemuri <svemuri8@gatech.edu>
2018-10-20 02:13:19 -0400

    Made ViewPager work. Add images to drawable. Add R.drawable.<image_name> to images array

--- app/src/main/java/com/nosleep/viewfinder/viewfinder/CustomPageAdapter.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/CustomPageAdapter.java
@@ -1,14 +1,19 @@
 package com.nosleep.viewfinder.viewfinder;
 
 import android.content.Context;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class CustomPageAdapter extends PagerAdapter {
 
     private Context mContext;
+    //Make firebase pass images into drawable and add references to this array
+
+    //array of all of the images
     private int[] images = {R.drawable.night_sky_1, R.drawable.night_sky_2,
             R.drawable.night_sky_3};
 
@@ -20,8 +25,10 @@ public class CustomPageAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup collection, int position) {
         int img = images[position];
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.fragment_night_sky_1,
+        ViewGroup layout = (ViewGroup) inflater.inflate(R.layout.fragment_cycle_images,
                 collection, false);
+        ImageView img_view = layout.findViewById(R.id.imageView);
+        img_view.setImageResource(img);
         collection.addView(layout);
         return layout;
     }
--- app/src/main/java/com/nosleep/viewfinder/viewfinder/cycle_images.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/cycle_images.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link night_sky_1.OnFragmentInteractionListener} interface
+ * {@link cycle_images.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link night_sky_1#newInstance} factory method to
+ * Use the {@link cycle_images#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class night_sky_1 extends Fragment {
+public class cycle_images extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class night_sky_1 extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public night_sky_1() {
+    public cycle_images() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class night_sky_1 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment night_sky_1.
+     * @return A new instance of fragment cycle_images.
      */
     // TODO: Rename and change types and number of parameters
-    public static night_sky_1 newInstance(String param1, String param2) {
-        night_sky_1 fragment = new night_sky_1();
+    public static cycle_images newInstance(String param1, String param2) {
+        cycle_images fragment = new cycle_images();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class night_sky_1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_night_sky_1, container, false);
+        return inflater.inflate(R.layout.fragment_cycle_images, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
