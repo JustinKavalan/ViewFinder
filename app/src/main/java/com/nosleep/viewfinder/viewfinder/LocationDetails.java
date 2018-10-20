29e8181bf392987d98bada718171d7e9c044ae72
Siddhartha Vemuri <svemuri8@gatech.edu>
2018-10-20 07:30:37 -0400

    Removed feed activity and added everything to MainActivity

--- app/src/main/java/com/nosleep/viewfinder/viewfinder/LocationDetails.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/LocationDetails.java
@@ -3,6 +3,8 @@ package com.nosleep.viewfinder.viewfinder;
 import android.app.Activity;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
 public class LocationDetails extends Activity {
@@ -12,7 +14,9 @@ public class LocationDetails extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_details);
 
-        // Get the intent that started this activity
         Intent intent = getIntent();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vp_location_images);
+        viewPager.setAdapter(new CustomPageAdapter(this));
     }
 }
