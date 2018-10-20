8bef8d4c62935d1c5ea926686effdbd1926753a7
Siddhartha Vemuri <svemuri8@gatech.edu>
2018-10-19 22:54:07 -0400

    Link LocationDetails to parent MainActivity

--- app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
+++ app/src/main/java/com/nosleep/viewfinder/viewfinder/MainActivity.java
@@ -1,7 +1,9 @@
 package com.nosleep.viewfinder.viewfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when user selects location from feed */
+    public void getLocationDetails(View view) {
+        Intent intent = new Intent(this, LocationDetails.class);
+        startActivity(intent);
     }
 }
